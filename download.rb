fc63ae5f07e2b52d8169c2f16336b7f4e2714065
weihai.tang <twh_eastasia@163.com>
2017-09-09 07:31:14 +0800

    update

--- download.rb
+++ download.rb
@@ -1,5 +1,18 @@
 # -- coding: UTF-8 --·
+require 'json'
 
+bundle_json_data = File.read('bundle_ids.json')
+json = JSON.parse(bundle_json_data)
+puts json[0]
+
+type_dict = {
+    "png": ["img",150],
+    "jpg": ["img",150],
+    "mp3": ["sound",20],
+    "mp4": ["video",10],
+    "gif": ["img",150],
+    "swf": ["flash", 3]
+}
 
 def download_from_url url, dir_name
   puts url
@@ -8,10 +21,16 @@ def download_from_url url, dir_name
   #`wget -r -A.png #{url}`
 end
 
+
 puts Time.new
-download_from_url 'http://www.beta.azj.saybot.net/images/EnglishLogo.png1', '中文目录'
-#download_from_url 'https://walle.dev.saybot.net', '中文目录'
-puts $?.exitstatus
+#download_from_url 'http://www.baidu.com/EnglishLogo.png1', '中文目录'
+
+#puts $?.exitstatus
 puts Time.new
 
-
+json.each do |bundle_id|
+  dname = bundle_id['name'].sub(' ', '')
+  puts dname
+  base_url = 'http://img-courseware.aaa.com/'+ bundle_id['bundle_id'] + '/data/'
+  puts base_url
+end
